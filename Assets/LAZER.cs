738e24e36ce61a0184b1aca8402f59958cea0ae6
adam.sanche <hooberschmit@gmail.com>
2019-01-27 14:30:01 -0700

    lerpylazer

--- Assets/LAZER.cs
+++ Assets/LAZER.cs
@@ -1,13 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class LAZER : MonoBehaviour
 {
     [SerializeField] private LineRenderer lineRenderer;
     [SerializeField] private GameObject lazer;
     [SerializeField] private GameObject particles;
-    [SerializeField] private float frequencyOfMovement = 5f;
 
-    private float lastTime = 0;
+    private bool lerping;
 
     private void Start()
     {
@@ -16,26 +16,52 @@ public class LAZER : MonoBehaviour
 
     private void Update()
     {
-        if (Time.time - lastTime > frequencyOfMovement)
+        if (!lerping)
         {
             var vectorOnUnitSphere = Random.onUnitSphere;
             vectorOnUnitSphere.y = -Mathf.Abs(vectorOnUnitSphere.y);
 
-            lazer.transform.LookAt(transform.position + (vectorOnUnitSphere * 20));
             var ray = new Ray(lineRenderer.transform.position, vectorOnUnitSphere);
             RaycastHit hitInfo;
             Physics.Raycast(ray, out hitInfo);
+
+            if (hitInfo.collider.gameObject != lazer)
+            {
+                StartCoroutine(SlerpyDerpy(hitInfo.point));
+            }
+        }
+    }
+
+    private IEnumerator SlerpyDerpy(Vector3 destinationPosition)
+    {
+        lerping = true;
+        if (!particles.activeSelf)
+        {
+            particles.SetActive(true);
+        }
+
+        var startTime = Time.time;
+
+        while (Vector3.Distance(lineRenderer.GetPosition(1), destinationPosition) > 0.05)
+        {
+            var currentPosition = lineRenderer.GetPosition(1);
+            var timeSinceStarted = Time.time - startTime;
+
+            var newPosition = Vector3.Lerp(
+                currentPosition,
+                destinationPosition,
+                Time.deltaTime * timeSinceStarted);
+
+            lazer.transform.LookAt(newPosition);
             lineRenderer.SetPosition(0, lineRenderer.transform.position);
-            lineRenderer.SetPosition(1, hitInfo.point);
-            particles.transform.position = hitInfo.point;
+            lineRenderer.SetPosition(1, newPosition);
+            particles.transform.position = newPosition;
             particles.transform.LookAt(lineRenderer.transform.position);
 
-            if (!particles.activeSelf)
-            {
-                particles.SetActive(true);
-            }
-
-            lastTime = Time.time;
+            yield return null;
         }
+
+        lineRenderer.SetPosition(1, destinationPosition);
+        lerping = false;
     }
 }
